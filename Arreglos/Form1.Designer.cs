1714800f476ba72a9b0f79d5362c64a7a8705058
eherca0502 <eduardoicc@outlook.es>
2023-02-20 19:40:40 -0600

    Agregar archivos de proyecto.

---
+++ Arreglos/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Arreglos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbCorreo = new System.Windows.Forms.TextBox();
+            this.tbTelefono = new System.Windows.Forms.TextBox();
+            this.tbApellido = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dgvContactos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(29, 32);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(65, 20);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre";
+            this.lblNombre.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.Location = new System.Drawing.Point(29, 65);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(65, 20);
+            this.lblApellido.TabIndex = 1;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefono.Location = new System.Drawing.Point(29, 106);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(71, 20);
+            this.lblTelefono.TabIndex = 2;
+            this.lblTelefono.Text = "Telefono";
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCorreo.Location = new System.Drawing.Point(29, 142);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(57, 20);
+            this.lblCorreo.TabIndex = 3;
+            this.lblCorreo.Text = "Correo";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNombre.Location = new System.Drawing.Point(92, 32);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(100, 26);
+            this.tbNombre.TabIndex = 4;
+            // 
+            // tbCorreo
+            // 
+            this.tbCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCorreo.Location = new System.Drawing.Point(92, 138);
+            this.tbCorreo.Name = "tbCorreo";
+            this.tbCorreo.Size = new System.Drawing.Size(100, 26);
+            this.tbCorreo.TabIndex = 5;
+            // 
+            // tbTelefono
+            // 
+            this.tbTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTelefono.Location = new System.Drawing.Point(106, 103);
+            this.tbTelefono.Name = "tbTelefono";
+            this.tbTelefono.Size = new System.Drawing.Size(100, 26);
+            this.tbTelefono.TabIndex = 6;
+            // 
+            // tbApellido
+            // 
+            this.tbApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbApellido.Location = new System.Drawing.Point(92, 64);
+            this.tbApellido.Name = "tbApellido";
+            this.tbApellido.Size = new System.Drawing.Size(100, 26);
+            this.tbApellido.TabIndex = 7;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(355, 28);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(93, 33);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(355, 67);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(93, 33);
+            this.btnBorrar.TabIndex = 9;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(355, 106);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(93, 36);
+            this.btnGuardar.TabIndex = 10;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // dgvContactos
+            // 
+            this.dgvContactos.AllowUserToAddRows = false;
+            this.dgvContactos.AllowUserToDeleteRows = false;
+            this.dgvContactos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContactos.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvContactos.Location = new System.Drawing.Point(61, 187);
+            this.dgvContactos.Name = "dgvContactos";
+            this.dgvContactos.ReadOnly = true;
+            this.dgvContactos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvContactos.Size = new System.Drawing.Size(521, 150);
+            this.dgvContactos.TabIndex = 11;
+            this.dgvContactos.DoubleClick += new System.EventHandler(this.dgvContactos_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvContactos);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.tbApellido);
+            this.Controls.Add(this.tbTelefono);
+            this.Controls.Add(this.tbCorreo);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.lblCorreo);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbCorreo;
+        private System.Windows.Forms.TextBox tbTelefono;
+        private System.Windows.Forms.TextBox tbApellido;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dgvContactos;
+    }
+}
+
---
+++ Arreglos/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Arreglos
+{
+    public partial class Form1 : Form
+    {
+        private List<Contacto> Contactos= new List<Contacto>();
+        private int indice = -1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader lector = new StreamReader("Agenda.txt");
+                string linea;
+                while((linea=lector.ReadLine())!=null)
+                {
+                    int posicion;
+                    Contacto persona = new Contacto();
+                    posicion = linea.IndexOf("|");
+                    persona.Nombre = linea.Substring(0, posicion);
+                    linea=linea.Substring(posicion+1);
+                    posicion =linea.IndexOf("|");
+                    persona.Apellido =linea.Substring(0, posicion);
+                    linea = linea.Substring(posicion+1);
+                    posicion =linea.IndexOf("|");
+                    persona.Telefono =linea.Substring(0, posicion);
+                    linea = linea.Substring(posicion + 1);
+                    posicion = linea.IndexOf('|');
+                    persona.Correo = linea.Substring(0, posicion);
+                    Contactos.Add(persona);
+                }
+                lector.Close();
+                actualizaVista();
+
+            }
+            catch(Exception ec) 
+            {
+                Console.WriteLine("Exception: " + ec.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Ejecucion finalizada");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Contacto persona = new Contacto();
+            persona.Nombre=tbNombre.Text;
+            persona.Apellido=tbApellido.Text;
+            persona.Telefono=tbTelefono.Text;
+            persona.Correo=tbCorreo.Text;
+            if(indice > -1)
+            {
+                Contactos[indice] = persona;
+                indice = -1;
+            }
+            else
+            {
+                Contactos.Add(persona);
+            }
+           
+            actualizaVista();
+            limpiaCampos();
+        }
+        private void actualizaVista()
+        {
+            dgvContactos.DataSource = null;
+            dgvContactos.DataSource = Contactos;
+            dgvContactos.ClearSelection();
+        }
+
+        private void dgvContactos_DoubleClick(object sender, EventArgs e)
+        {
+            DataGridViewRow renglon = dgvContactos.SelectedRows[0];
+             indice =dgvContactos.Rows.IndexOf(renglon);
+            Contacto persona = Contactos[indice];
+            tbNombre.Text = persona.Nombre;
+            tbApellido.Text = persona.Apellido;
+            tbTelefono.Text=persona.Telefono;
+            tbCorreo.Text = persona.Correo;
+
+        }
+        private void limpiaCampos()
+        {
+            tbNombre.Text = null;
+            tbApellido.Text = null;
+            tbTelefono.Text = null;
+            tbCorreo.Text = null;
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if(indice > -1)
+            {
+                Contactos.RemoveAt(indice);
+                actualizaVista();
+                limpiaCampos();
+                indice = -1;
+            }
+            else
+            {
+                MessageBox.Show("Selecciona el registro que deseas borrar");
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            TextWriter Escribir = new StreamWriter("Agenda.txt");
+            foreach(Contacto persona in Contactos)
+            {
+                Escribir.WriteLine(persona.Nombre+ "|"+ persona.Apellido+ "|"+persona.Telefono+ "|"+persona.Correo+ "|");
+            }
+            Escribir.Close();
+            MessageBox.Show("Contactos Guardados");
+
+        }
+    }
+}
